0f78cff107f4549affd7d64c00401f35e7a4cfcf
thill1 <thill1@users.noreply.github.com>
2026-01-03 00:34:48 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  base: './', // Ensures assets are linked relatively (e.g., for GitHub Pages repo paths)
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  base: process.env.GITHUB_ACTIONS
+    ? `/${process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""}/`
+    : "/",
 });
