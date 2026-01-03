ccfec0cbbcf4495949254a1ea532f62017cb80cd
thill1 <thill1@users.noreply.github.com>
2026-01-03 02:16:40 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages project site base (repo name = LordsGym)
   base: "/LordsGym/",
 });
