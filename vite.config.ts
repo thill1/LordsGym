75c1056f19cdcf7c96c7ab3a36f7f27ea0df3673
thill1 <thill1@users.noreply.github.com>
2026-01-04 16:28:06 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages project site base path:
+
+  // ✅ GitHub Pages project site base path:
   // https://thill1.github.io/LordsGym/
   base: "/LordsGym/",
+
   build: {
     outDir: "dist",
     emptyOutDir: true,
